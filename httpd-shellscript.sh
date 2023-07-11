6c331e86fdde647c20fe4d2f83551631e91d4eb1
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-07-11 16:02:04 +0530

    Create httpd-shellscript.sh

---
+++ httpd-shellscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install Apache HTTP Server
+sudo yum update -y
+sudo yum install httpd -y
+
+# Start the Apache service
+sudo systemctl start httpd
+
+# Enable Apache to start on system boot
+sudo systemctl enable httpd
+
+# Copy the index.html file
+sudo cp index.html /var/www/html/
+
+# Restart Apache to apply changes
+sudo systemctl restart httpd

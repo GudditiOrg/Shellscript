847f5656e29a727ea72c89fc3296382e5e053cd3
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-07-12 15:27:01 +0530

    Update docker-shellscript.sh

--- docker-shellscript.sh
+++ docker-shellscript.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+# Install docker
+sudo yum install docker -y
+
+# Enable and start service
+sudo systemctl enable docker
+sudo systemctl start docker
+
 # Build the Docker image
 docker build -t httpd-image .
 
 # Run a container from the image
-docker run -d -p 80:80 --name my-httpd-container httpd-image
+docker run -it -p 80:80 --name my-httpd-container httpd-image

b3b92f3d215e9aec8bfce6c9ecc63c042aa50984
GudditiNaganjaneyulu <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-07-13 15:42:05 +0530

    Create linux-todolist.sh

---
+++ linux-todolist.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install Apache HTTP Server
+sudo yum update -y
+sudo yum install httpd -y
+
+# Start the Apache service
+sudo systemctl start httpd
+
+# Enable Apache to start on system boot
+sudo systemctl enable httpd
+
+# Moving file linux-todolist.html -> index.html
+sudo mv  linux-todolist.html  index.html
+# Copy the index.html file
+sudo cp index.html /var/www/html/
+
+# Restart Apache to apply changes
+sudo systemctl restart httpd
+
+# get the service status
+netstat -tlpn |grep "httpd"
